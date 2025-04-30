0bd5d3f76dabd9059d0f47f185a63124f43bc43c
Cheesky <ruby.akbar.firmansyah@gmail.com>
2025-04-30 18:19:22 +0700

    Adding reverse function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 var input = 0;
 var output = 0;
-var reversed = False;
+var opsi = false;
+document.getElementById("label-input").innerHTML = "Celcius (&deg;C)";
+document.getElementById("label-output").innerHTML = "Fahrenheit (&deg;F)";
 
-function convert(){
-    input = document.getElementById("input").value;
-    output = (input * 1.8) + 32;
-    alert(output);
-    document.getElementById("result").innerHTML = output;
-    document.getElementById("result-sidepanel").innerHTML = output;
-}
-
-function reverse() {
-	reversed = !reversed;
-	if(reversed = True){
+function reverse(){
+    opsi = !opsi;
+    if(opsi == true){
         document.getElementById("label-input").innerHTML = "Fahrenheit (&deg;F)";
         document.getElementById("label-output").innerHTML = "Celcius (&deg;C)";
     }
     else{
-        document.getElementById("label-output").innerHTML = "Fahrenheit (&deg;F)";
         document.getElementById("label-input").innerHTML = "Celcius (&deg;C)";
+        document.getElementById("label-output").innerHTML = "Fahrenheit (&deg;F)";
+    }
+    return opsi;
+}
+function convert(){
+    alert(opsi);
+    if(opsi === true){
+        // Fahrenheit to Celcius
+        input = document.getElementById("input").value;
+        output = (input - 32)/1.8;
+        document.getElementById("result-sidepanel").innerHTML = output;
+        document.getElementById("result").innerHTML = output;
+    }
+    else{
+        // Celcius to Fahrenheit
+        input = document.getElementById("input").value;
+        output = (input * 1.8)+32;
+        document.getElementById("result-sidepanel").innerHTML = output;
+        document.getElementById("result").innerHTML = output;
     }
 }
