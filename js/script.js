0a641015a2ff9ff6f0ec4e12fb96d1ecb119d98e
Cheesky <ruby.akbar.firmansyah@gmail.com>
2025-04-30 21:44:02 +0700

    Added result color functionality

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ function convert(){
                                                                 "S<sub>(&deg;C)</sub> = ("+input+"- 32) / 1.8<br>"+
                                                                 "S<sub>(&deg;C)</sub> = ("+(input - 32)+") / 1.8<br>"+
                                                                 "S<sub>(&deg;C)</sub> = ("+(input - 32)/1.8+")";
+        return tempcolor(input)
     }
     else{
         // Celcius to Fahrenheit
@@ -41,14 +42,64 @@ function convert(){
                                                                 "S<sub>(&deg;F)</sub> = ("+input+"&times; 1,8) + 32<br>"+
                                                                 "S<sub>(&deg;F)</sub> = ("+(input * 1.8)+") + 32<br>"+
                                                                 "S<sub>(&deg;F)</sub> = ("+(input * 1.8 + 32)+")";
+        return tempcolor(output);
     }
 }
-if(output <= -51){
-    // Unearthly Cold color rgba(60,49,151,255)
-}
-else if(output<=-21){
-    //Extreme Cold color rgba(38,0,207,255)
-}
-else if(output <=0){
-    //Severe Cold rgba(71,98,251,255)
+
+function tempcolor(){
+    if(output <= -51){
+        // Unearthly Cold color rgba(60,49,151,255)
+        document.getElementById("status").innerHTML = "Unearthly Cold";
+        document.getElementById("result-color").style.backgroundColor = "rgba(60,49,151,255)";
+    }
+    else if(output<=-21){
+        //Extreme Cold color rgba(38,0,207,255)
+        document.getElementById("status").innerHTML = "Extreme Cold";
+        document.getElementById("result-color").style.backgroundColor = "rgba(38,0,207,255)";
+    }
+    else if(output <=0){
+        //Severe Cold rgba(71,98,251,255)
+        document.getElementById("status").innerHTML = "Severe Cold";
+        document.getElementById("result-color").style.backgroundColor = "rgba(71,98,251,255)";
+    }
+    else if(output <=40){
+        //Cold rgba(159,202,253,255)
+        document.getElementById("status").innerHTML = "Cold";
+        document.getElementById("result-color").style.backgroundColor = "rgba(159,202,253,255)";
+    }
+    else if(output <=60){
+        //Moderate rgba(150,204,23,255)
+        document.getElementById("status").innerHTML = "Moderate";
+        document.getElementById("result-color").style.backgroundColor = "rgba(150,204,23,255)";
+    }
+    else if(output <=91){
+        //Warm rgba(250,208,36,255)
+        document.getElementById("status").innerHTML = "Warm";
+        document.getElementById("result-color").style.backgroundColor = "rgba(250,208,36,255)";
+    }
+    else if(output <=110){
+        //Hot rgba(250,160,34,255)
+        document.getElementById("status").innerHTML = "Hot";
+        document.getElementById("result-color").style.backgroundColor = "rgba(250,160,34,255)";
+    }
+    else if(output <=140){
+        //Severe Heat rgba(250,110,35,255)
+        document.getElementById("status").innerHTML = "Severe Heat";
+        document.getElementById("result-color").style.backgroundColor = "rgba(250,110,35,255)";
+    }
+    else if(output <=180){
+        //Extreme Heat rgba(150,58,21,255)
+        document.getElementById("status").innerHTML = "Extreme Heat";
+        document.getElementById("result-color").style.backgroundColor = "rgba(150,58,21,255)";
+    }
+    else if(output <=210){
+        //Unearthly Heat rgba(139,25,15,255)
+        document.getElementById("status").innerHTML = "Unearthly Heat";
+        document.getElementById("result-color").style.backgroundColor = "rgba(139,25,15,255)";
+    }
+    else{
+        //Burning Heat black
+        document.getElementById("status").innerHTML = "Burning Heat";
+        document.getElementById("result-color").style.backgroundColor = "black";
+    }
 }
